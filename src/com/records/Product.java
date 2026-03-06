85ae60ca1b208eaa9275730ad91a5a25449eb15b
Pranay1012904 <pranay.dmnsns@gmail.com>
2026-03-06 16:51:11 +0530

    added validations for parameters in records, verified via test cases

--- src/com/records/Product.java
+++ src/com/records/Product.java
@@ -1,6 +1,20 @@
 package com.records;
 
+import org.junit.platform.commons.util.StringUtils;
+
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public record Product(String name, BigDecimal cost, String type) {
+
+    //implementing validation
+    public Product{
+        if(name==null || name.isBlank()){
+            throw new IllegalArgumentException("Invalid name!");
+        }
+
+        if(cost.compareTo(BigDecimal.ZERO)<=0){
+            throw new IllegalArgumentException("Invalid Cost Moron!");
+        }
+    }
 }
--- src/test/ProductTest.java
+++ src/test/ProductTest.java
@@ -6,12 +6,30 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ProductTest {
 
     @Test
-    void createProduct(){
-        var product=new Product("Jackson",new BigDecimal(89.99),"guitar");
-        Assertions.assertEquals("guitar",product.type());
-        Assertions.assertEquals("Jackson",product.name());
+    void createProduct() {
+        var product = new Product("Jackson", new BigDecimal(89.99), "guitar");
+        assertEquals("guitar", product.type());
+        assertEquals("Jackson", product.name());
+    }
+
+    @Test
+    void testInvalidName() {
+        //var product=new Product(null,new BigDecimal(89.99),"guitar");
+        var ex1 = assertThrows(IllegalArgumentException.class, () -> new Product(" ", new BigDecimal(89.99), "guitar"));
+        var ex2 = assertThrows(IllegalArgumentException.class, () -> new Product(null, new BigDecimal(89.99), "guitar"));
+        assertEquals("Invalid name!", ex1.getMessage());
+        assertEquals("Invalid name!", ex2.getMessage());
+    }
+
+    @Test
+    void testInvalidCost() {
+        var ex = assertThrows(IllegalArgumentException.class, () -> new Product("B.C. Rich", new BigDecimal(0.0), "Guitar"));
+        assertEquals("Invalid Cost Moron!",ex.getMessage());
     }
 }
